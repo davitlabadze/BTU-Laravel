4c337854614aaeee57eaf5bb2d40691e943e002f
davitlabadze <labadzedato18@gmail.com>
2022-01-02 12:53:10 +0400

    create welcome and dashboard connect mailtrap

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,17 +3,25 @@
 
     <BreezeAuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+                            <Select2
+                            v-model="myValue"
+                            :options="myOptions"
+                            :settings="{ width:'60%' }"
+                            @change="myChangeEvent($event)"
+                            @select="mySelectEvent($event)"
+                            />
+                            <h4>Value: {{ myValue }}</h4>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -23,11 +31,26 @@
 <script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head } from '@inertiajs/inertia-vue3';
-
+import Select2 from 'vue3-select2-component';
 export default {
     components: {
         BreezeAuthenticatedLayout,
         Head,
+        Select2,
     },
+    data() {
+        return {
+            myValue: '',
+            myOptions: ['საქართველო', 'German', 'English'],
+        }
+    },
+    methods: {
+        myChangeEvent(val){
+            console.log(val);
+        },
+        mySelectEvent({id, text}){
+            console.log({id, text})
+        }
+    }
 }
 </script>
