72befd877c848c149c105b9f333f6555c98f9b2b
davitlabadze <labadzedato18@gmail.com>
2022-01-12 01:58:53 +0400

    add countryseeder

--- database/migrations/2022_01_10_212556_create_countries_table.php
+++ database/migrations/2022_01_10_212556_create_countries_table.php
@@ -16,6 +16,7 @@ class CreateCountriesTable extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('code');
             $table->timestamps();
         });
     }
