9b5365c296acfa5e0410e2b74cb9311b4c6b5010
davitlabadze <labadzedato18@gmail.com>
2022-01-12 00:50:34 +0400

    add dashboard and welcome page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,31 +3,140 @@
 
     <BreezeAuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="flex">
+                        <div class="w-1/2">left side</div>
+                        <div class="w-1/2">
+                            <div class="p-2 rounded shadow">
+                                <h3 class="mt-2 text-xl text-center text-indigo-500">Visited Coutry</h3>
+                                <div class="flex p-6 bg-white border-b border-gray-200">
+                                   <div class="flex">
+                                    <Select2
+                                    class=" w-96"
+                                    v-model="SelectedVisitedCuntry"
+                                    :settings="{
+                                        width:'100%',
+                                        ajax: {
+                                            url: '/api/countries/list',
+                                            dataType: 'json'
+                                        },
+                                    }"
+
+                                    @select="VisitedCoutrySelected($event)"
+                                    />
+                                    <a href="javascript:void(0);"
+                                    @click="addVisitedCountry"
+                                    class="px-1 py-3 ml-3 text-xl font-semibold text-white bg-green-400 rounded shadow-xl hover:bg-green-500 ">ADD</a>
+                                   </div>
+
+                                    <div v-for="vr in visitedCountries" :key="vr.id0" class="mt-5 ">
+                                        <span class="block w-full pt-3 mb-1 border-b-2">{{ vr.name }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 rounded shadow">
+                                <h3 class="mt-2 text-xl text-center text-indigo-500">Countries to visit</h3>
+                                <div class="p-6 bg-white border-b border-gray-200">
+                                    <Select2
+                                    v-model="SelectedCountryToVisit"
+                                    :settings="{
+                                        width:'100%',
+                                        ajax: {
+                                            url: '/api/countries/list',
+                                            dataType: 'json'
+                                        },
+                                    }"
+
+                                    @select="CountryToVisitSelected($event)"
+                                    />
+                                     <div v-for="ctv in countriesToVisit" :key="ctv.id0" class="mt-5 ">
+                                        <span class="block w-full pt-3 mb-1 border-b-2">{{ ctv.name }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
+
     </BreezeAuthenticatedLayout>
 </template>
 
 <script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head } from '@inertiajs/inertia-vue3';
-
+import Select2 from 'vue3-select2-component';
+import axios from 'axios';
 export default {
     components: {
         BreezeAuthenticatedLayout,
         Head,
+        Select2,
     },
+    data() {
+        return {
+            SelectedVisitedCuntry: '',
+            SelectedCountryToVisit:'',
+            visitedCountries: [],
+            countriesToVisit:[],
+        }
+    },
+    methods: {
+        VisitedCoutrySelected({id, text}){
+            // console.log({id, text})
+        },
+        CountryToVisitSelected({id, text}){
+            // console.log({id, text})
+        },
+        getVisitedCoutry(){
+            axios.get('/api/countries/visited')
+            .then((response) => {
+                this.visitedCoutries = response.data;
+            });
+        },
+        getToVisitCoutry(){
+            axios.get('/api/countries/tovisit')
+                        .then((response) => {
+                            this.countriesToVisit = response.data;
+                        });
+        },
+        addVisitedCountry(){
+            if (this.SelectedVisitedCuntry !== '') {
+                axios.post('/api/add-visited-country',{
+                    countryID: this.SelectedVisitedCuntry
+                }).then((response) => {
+                    this.getVisitedCoutry();
+                }).catch(function(error){
+                    console.log(error)
+                })
+            }else{
+                console.log('error');
+            }
+        }
+    },
+    created(){
+        this.getVisitedCoutry();
+        this.getToVisitCoutry();
+    }
 }
 </script>
+
+
+
+
+
+
+
+
+
+
